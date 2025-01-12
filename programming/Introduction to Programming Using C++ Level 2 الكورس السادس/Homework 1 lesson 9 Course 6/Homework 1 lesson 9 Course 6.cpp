21ed52424c52f057220f6efad86517a6fbe29606
Ahmed <ahmed23156p@gmail.com>
2025-01-12 20:53:19 +0400

    oh man

---
+++ programming/Introduction to Programming Using C++ Level 2 الكورس السادس/Homework 1 lesson 9 Course 6/Homework 1 lesson 9 Course 6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int Number = -1;
+	string Result;
+	Result = (Number > 0) ? "Positive" : "Negative";
+	cout << "Number is " << Result << endl;
+	return 0;
+}
